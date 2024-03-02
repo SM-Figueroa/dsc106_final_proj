e5d432d3cf0ef481e67966d80d1b62e3346e756a
SM-Figueroa <s2figuer@ucsd.edu>
2024-03-01 19:34:43 -0800

    style: add interaction descriptions

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -26,7 +26,28 @@
   <h1>DSC106 Final Project Prototype</h1>
   <h3>Country Clustering</h3>
 
+  <p>
+    Visualized below is a dataset of livelihood statistics, 
+    where each point on the scatter plot represents a country. 
+    Change the x and y variables to visualize unique trends between these statistics.
+  </p>
+
+  <p>
+    There's also an option to set a number of components or groups to cluster the data by. 
+    After setting this value you can click on any point on the graph to draw centroids. 
+  </p>
+
+  <p>
+    In the future, we plan use the choices of variables and centroids to perform a clustering algorithm
+    that will color code the points of the scatter plot by each cluster.
+  </p>
+
   <Scatter {data}/>
+
+
+  <p>
+    Here (below the scatter plot) we will include a map, with countries shaded by color of cluster determined from the previous graphic.
+  </p>
 </main>
 
 <style>
