620c92166f4896f712309b3ef36df987402fb01c
SM-Figueroa <s2figuer@ucsd.edu>
2024-03-01 17:56:06 -0800

    task: add Scatter component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,32 @@
 <script>
-  import Simulate from "./Simulate.svelte";
-  console.log("Hello from App.svelte");
+  import Scatter from "./Scatter.svelte";
+  import * as d3 from 'd3';
+  import { onMount } from 'svelte';
+
+
+  // load dataframe here
+    let data = [];
+
+    onMount(async () => {
+
+        const res = await fetch('https://raw.githubusercontent.com/SM-Figueroa/dsc106_final_proj/main/static/country_data_transformed.csv'); 
+
+        const csv = await res.text();
+
+        data = d3.csvParse(csv, d3.autoType);
+
+        console.log(data);
+
+    });
+
+
 </script>
 
 <main>
   <h1>DSC106 Final Project Prototype</h1>
-  <h3>Data Representation Playground</h3>
+  <h3>Country Clustering</h3>
 
-  <Simulate />
+  <Scatter {data}/>
 </main>
 
 <style>
