eab000b87e1ae966083d3a2f9293e2aee59e50ff
Annu Indraganti <aindraga@ucsd.edu>
2024-03-07 16:59:04 -0800

    Map Svelte File

---
+++ src/components/Map.svelte
@@ -0,0 +1,13 @@
+const svg = d3.select("svg");
+d3.json("component\\world_map.geo.json").then(function(geojson) {
+    const projection = d3.geoMercator().fitSize([800, 600], geojson);
+    const path = d3.geoPath().projection(projection);
+
+    svg.selectAll("path")
+        .data(geojson.features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("fill", "steelblue")
+        .attr("stroke", "white")
+        .attr("stroke-width", 0.5)
+});
