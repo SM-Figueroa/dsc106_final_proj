3bc6c062ff3d472535785a134ac2006de050f621
Annu Indraganti <aindraga@ucsd.edu>
2024-03-10 16:57:23 -0700

    Colored Map Commit

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -7,6 +7,9 @@
     onMount(async () => {
       const response = await fetch('https://raw.githubusercontent.com/SM-Figueroa/dsc106_final_proj/main/static/world_map.geo.json');
       const data = await response.json();
+
+      const clusters_response = await fetch('https://raw.githubusercontent.com/SM-Figueroa/dsc106_final_proj/main/static/clustered_data_with_country.json');
+      const clusters_data = await clusters_response.json()
   
       const width = 960;
       const height = 500;
@@ -21,12 +24,20 @@
         .append('svg')
         .attr('width', width)
         .attr('height', height);
+
+        const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+        const countryClusterMap = {};
+        clusters_data.forEach(cluster => {
+          cluster.countries.forEach(country => {
+            countryClusterMap[country] = cluster.cluster;
+          });
+        });
   
       svg.selectAll('path')
         .data(data.features)
         .enter().append('path')
         .attr('d', path)
-        .attr('fill', 'lightgray')
+        .attr('fill', d => colorScale(countryClusterMap[d.properties.name]))
         .attr('stroke', 'white');
     });
   </script>
