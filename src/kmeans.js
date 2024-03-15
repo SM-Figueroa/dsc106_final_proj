5c48a9e2be3b441ed3d4e0ee277742bcc5a9c6a5
Vikram Venkatesh <vvenkatesh@ucsd.edu>
2024-03-15 14:27:22 -0700

    Update kmeans.js

--- src/kmeans.js
+++ src/kmeans.js
@@ -1,7 +1,3 @@
-// import fs from 'fs';
-// import csv from 'csv-parser';
-
-
 export function km(data, centroids, var1, var2) {
 
   // let centroids = init_centroids.slice();
@@ -15,36 +11,10 @@ export function km(data, centroids, var1, var2) {
 
   if (hasConverged(newCentroids, centroids)) {
       console.log("Converged");
+      return -1;
   }
 
   return newCentroids;
-}
-
-function kmeans(data, init_centroids, maxIterations = 1000) {
-    let centroids = init_centroids.slice(); // Copy the initial centroids
-    let prevCentroids = [centroids.map(centroid => centroid.slice())]; // Store initial centroids
-    let clusters; // Initialize clusters here
-
-    for (let iter = 0; iter < maxIterations; iter++) {
-        // Assign points to clusters
-        clusters = assignPointsToClusters(data, centroids);
-
-        // Calculate new centroids
-        const newCentroids = calculateCentroids(clusters);
-        if (hasConverged(newCentroids, centroids)) {
-            break;
-        }
-        
-        prevCentroids.push(newCentroids.map(centroid => centroid.slice())); // Store unique previous centroids
-        centroids = newCentroids;
-    }
-
-    // Filter out duplicate previous centroids
-    prevCentroids = prevCentroids.filter((centroid, index) => 
-        index === prevCentroids.findIndex(c => arraysEqual(c, centroid))
-    );
-
-    return { clusters, centroids, all_centroids: prevCentroids };
 }
 
 function hasConverged(centroids, prevCentroids) {
@@ -112,59 +82,3 @@ function calculateCentroids(clusters, var1, var2) {
       return centroid;
     });
 }
-
-function arraysEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) return false;
-    }
-    return true;
-}
-
-function visualize(col1, col2, init_centroids) {
-    const results = [];
-    const countryNames = []; // Array to store country names
-
-    fs.createReadStream('/Users/vikram/Documents/dsc106_final_proj/static/country_data_transformed.csv')
-        .pipe(csv())
-        .on('data', (row) => {
-            const exportsValue = parseFloat(row[col1]);
-            const healthValue = parseFloat(row[col2]);
-            const countryName = row['country'];
-
-            if (!isNaN(exportsValue) && !isNaN(healthValue)) {
-                results.push([exportsValue, healthValue]);
-                countryNames.push(countryName);
-            }
-        })
-        .on('end', () => {
-            // Perform k-means clustering
-            const { clusters, centroids, all_centroids } = kmeans(results, init_centroids);
-
-            // Append country names to the clusters
-            const clusteredDataWithCountry = clusters.map((cluster, index) => {
-                return {
-                    cluster: index + 1,
-                    countries: cluster.map((_, idx) => countryNames[results.indexOf(cluster[idx])])
-                };
-            });
-
-            // uncomment to save the output clusters as a json file
-            // const jsonContent = JSON.stringify(clusteredDataWithCountry, null, 2);
-
-            // fs.writeFile('clustered_data_with_country.json', jsonContent, 'utf8', (err) => {
-            //     if (err) {
-            //         console.error('An error occurred while writing JSON Object to File.');
-            //         return console.error(err);
-            //     }
-            //     console.log('JSON file has been saved.');
-            // });
-
-            // console.log("Centroids:", centroids);
-            // console.log("Previous Centroids", all_centroids);
-            // console.log("Clusters:", clusteredDataWithCountry);
-        });
-        return clusteredDataWithCountry;
-}
-
-// console.log(visualize('gdpp', 'imports', [[8430, 696.64], [16000, 1208.88], [32300, 9436]]));
