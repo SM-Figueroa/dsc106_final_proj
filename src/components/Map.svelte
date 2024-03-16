93bf749b205737f489fa9344ae9109565ee0b390
SM-Figueroa <s2figuer@ucsd.edu>
2024-03-16 03:44:48 -0700

    feat: map interaction with hover and color correction

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,22 +1,27 @@
 <script>
+  // imports/exports
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
   export let data;
-
+  export let mapSelect;
+  export let mapDeselect;
+  export let colorScale;
+  
+  // initial state
   let mapContainer;
   let svg;
   let clustersDict = {};
-  const colorScale = d3.scaleOrdinal()
-      .domain([0, 5])
-      .range(['#5AC8FA', '#FFA500', '#008080', '#EE82EE', '#483D8B']);
 
   onMount(async () => {
+    // fetch geojson
     const response = await fetch('https://raw.githubusercontent.com/SM-Figueroa/dsc106_final_proj/main/static/world_map.geo.json');
     const jsonData = await response.json();
 
+    // map dims
     const width = 960;
     const height = 500;
 
+    // create map
     const projection = d3.geoMercator()
       .translate([width / 2, height / 2])
       .scale(100);
@@ -28,25 +33,44 @@
       .attr("width", width)
       .attr("height", height);
 
+    // create paths with hover functionality
     svg.selectAll('path')
       .data(jsonData.features)
       .enter().append('path')
       .attr('d', path)
       .attr('fill', "grey")
-      .attr('stroke', 'white');
+      .attr('stroke', 'white')
+      .on('mouseover', function(event, d) {
+            const name = d.properties.name;
+            d3.select(this)
+              .attr('fill', brighten(clustersDict[name] > 0 ? colorScale(clustersDict[name]) : "gray"));
+            mapSelect(name);
+          })
+      .on('mouseout', function(event, d) {
+          const name = d.properties.name;
+          d3.select(this)
+            .attr('fill', clustersDict[name] > 0 ? colorScale(clustersDict[name]) : "gray");
+          mapDeselect(name);
+      })
+
+
   });
 
+  function brighten(color) {
+    return d3.color(color).brighter(0.7);
+  }
+  
+  // update map with cluster denominations
   $: {
     if (svg && data.length > 0) {
       for (let i = 0; i < data.length; i++) {
-        clustersDict[data[i]["country"]] = data[i]["cluster"];
+        clustersDict[data[i].country] = data[i]["cluster"];
       }
-      console.log(clustersDict);
 
       svg.selectAll('path')
-        .attr('fill', d => colorScale(clustersDict[d.properties.name]));
+        .attr('fill', d => clustersDict[d.properties.name] > 0 ? colorScale(clustersDict[d.properties.name]) : "gray");
     } else {
-      console.log("Initialized");
+      // console.log("Initialized");
     }
   }
 </script>
